af9cb0198fe16c641dab6e5934c3f9828f16e8a5
easonandkhalil <easonandkhalil@gmail.com>
2020-02-05 16:59:19 +0800

    增加一条输出

--- src/main/java/com/eason/learngit/demo/demo.java
+++ src/main/java/com/eason/learngit/demo/demo.java
@@ -9,5 +9,6 @@ package com.eason.learngit.demo;
 public class demo {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("origin");
     }
 }
