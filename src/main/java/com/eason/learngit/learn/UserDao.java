e3c88508271b47264447db6919d5c423d04b4410
easonandkhalil <easonandkhalil@gmail.com>
2020-02-19 19:00:44 +0800

    提交zhutianlong分支上的新增文件

---
+++ src/main/java/com/eason/learngit/learn/UserDao.java
@@ -0,0 +1,31 @@
+package com.eason.learngit.learn;
+
+import java.util.List;
+
+/**
+ * UserDao
+ *
+ * @author eason
+ * @date 2020/02/2020-02-16
+ */
+public class UserDao extends AbstractDao<User>{
+    public User getById(Integer id) {
+        return null;
+    }
+
+    public int insert(User user) {
+        return 0;
+    }
+
+    public int insertList(List list) {
+        return 0;
+    }
+
+    public int deleteById(Integer id) {
+        return 0;
+    }
+
+    public int deleteByIds(List list) {
+        return 0;
+    }
+}
--- src/main/java/com/eason/learngit/learn/demo.java
+++ src/main/java/com/eason/learngit/learn/demo.java
@@ -1,5 +1,8 @@
 package com.eason.learngit.learn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * demo
  *
@@ -12,5 +15,10 @@ public class demo {
         // System.out.println("origin");
         System.out.println("创建了zhutianloing分支");
         System.out.println("在zhutianlong分支上修改");
+        List<String> stringList = new ArrayList<String>();
+
+        User user = new User();
+        UserDao userDao = new UserDao();
+        userDao.getById(15);
     }
 }
